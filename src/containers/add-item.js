259e66f611c1bca956c116ea828fdcdb12feb109
Andrew Steinheiser <me@iamandrew.io>
2018-01-13 15:01:24 -0700

    forgot my add-item lol

---
+++ src/containers/add-item.js
@@ -0,0 +1,149 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { Button, Icon, Card, Subheader } from 'react-native-material-ui'
+import styled from 'styled-components/native'
+
+const BASE_URL = 'https://bljp0y84gh.execute-api.us-west-2.amazonaws.com/Hack'
+
+class AddItem extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      barcode: null,
+      newItem: null,
+      error: null
+    }
+  }
+
+  async componentWillMount() {
+    await this.setState({ barcode: this.props.navigation.state.params.barcode })
+
+    fetch(`${BASE_URL}/food?upc=${this.state.barcode}`, { method: 'GET' })
+      .then(response => response.json())
+      .then(responseJson => this.setState({ newItem: responseJson }))
+      .catch(error => this.setState({ error }))
+  }
+
+  addItemToInventory = () => {
+    console.log('Adding Item!')
+  }
+
+  backToInventory = () => {
+    console.log('Back to Inventory!')
+  }
+
+  render() {
+    return (
+      <Container>
+        <Card>
+          {
+            this.state.newItem ?
+            <View>
+              <HeaderMargin>
+                <Subheader text={'Item Found:'} />
+                <Margin>
+                  <Text>
+                    {this.state.newItem.name}
+                  </Text>
+                </Margin>
+              </HeaderMargin>
+              {
+                this.state.newItem.cautions ?
+                <MarginLeft>
+                  <Subheader text='Allergy Information:' />
+                  <Margin>
+                    <Text>
+                      { this.state.newItem.cautions.map((caution) => caution + "   " ) }
+                    </Text>
+                  </Margin>
+                </MarginLeft>
+                :
+                ''
+              }
+              {
+                this.state.newItem.healthLabels ?
+                <MarginLeft>
+                  <Subheader text='Diet Restrictions:' />
+                  <Margin>
+                    <Text>
+                      {
+                        this.state.newItem.healthLabels.map((label) => {
+                          if(label === 'VEGAN') {
+                            return label + "   "
+                          }
+                          if(label === 'VEGETARIAN') {
+                            return label + "   "
+                          }
+                          if(label === 'KOSHER') {
+                            return label + "   "
+                          }
+                        })
+                      }
+                    </Text>
+                  </Margin>
+                </MarginLeft>
+                :
+                ''
+              }
+              <TopMargin>
+                <WhiteBg>
+                  <Margin>
+                    <Button
+                      raised
+                      primary
+                      text="Add This Item"
+                      onPress={this.addItemToInventory} />
+                  </Margin>
+                </WhiteBg>
+                <WhiteBg>
+                  <Margin>
+                    <Button
+                      raised
+                      accent
+                      text="Back to Inventory"
+                      onPress={this.backToInventory} />
+                  </Margin>
+                </WhiteBg>
+              </TopMargin>
+            </View>
+            :
+            <LoadingMargin>
+              <Subheader text={ this.state.error ? error.message : 'Loading...'} />
+            </LoadingMargin>
+          }
+        </Card>
+      </Container>
+    )
+  }
+}
+export default AddItem
+
+const Container = styled.View`
+  flex: 1;
+  margin-top: 10px;
+  overflow: scroll;
+`
+
+const Margin = styled.View`
+  margin: 0 20px 20px 20px;
+`
+
+const WhiteBg = styled.View`
+  background-color: white;
+`
+
+const HeaderMargin = styled.View`
+  margin: 10px 10px 0 10px;
+`
+
+const LoadingMargin = styled.View`
+  margin: 10px;
+`
+
+const MarginLeft = styled.View`
+  margin-left: 10px;
+`
+
+const TopMargin = styled.View`
+  margin-top: 20px;
+`
